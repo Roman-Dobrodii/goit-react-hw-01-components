3ffe9b02f8a698b0a74bc2b626a70a1d1c44ad9e
Roman Dobrodii <romariosi007@gmail.com>
2020-12-28 00:11:05 +0200

    Добавляет решение первого задания без стилизации.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from './components/Profile/Profile';
+import user from './data-base/user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
