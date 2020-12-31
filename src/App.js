5ecb15629acbe42bfe68c37abea435ff648ea32b
Roman Dobrodii <romariosi007@gmail.com>
2020-12-31 16:00:14 +0200

    Добавляет разметку компонентов

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Profile from './components/Profile/Profile';
 import user from './data-base/user.json';
+import statisticalData from './data-base/statistical-data.json';
+import friends from './data-base/friends.json';
+import transactions from './data-base/transactions.json';
 
 export default function App() {
   return (
